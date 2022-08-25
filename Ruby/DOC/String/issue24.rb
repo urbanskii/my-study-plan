8ab28665be664201b84c3640c9373bd1b0f6c769
urbanskii <marcos.urbanski@gmail.com>
2022-08-25 19:14:53 -0300

    add file issue24.rb close #24

---
+++ Ruby/DOC/String/issue24.rb
@@ -0,0 +1,5 @@
+# bytesize → integerclick to toggle source, Returns the count of bytes in self:
+
+puts 'Returns the count of bytes in self:'
+puts  "\x80\u3042".bytesize # => 4
+puts "hello".bytesize # => 5
