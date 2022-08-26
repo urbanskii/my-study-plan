2d877c045f4d556803219124ea98a4f7784c83d8
urbanskii <marcos.urbanski@gmail.com>
2022-08-26 20:55:49 -0300

    add file issue26.rb

---
+++ Ruby/DOC/String/issue26.rb
@@ -0,0 +1,17 @@
+=begin
+
+
+capitalize!(*options) → self or nil
+
+Upcases the first character in self; downcases the remaining characters; returns self if any changes were made, nil otherwise:
+
+
+=end
+
+puts ' Upcases the first character in self; downcases the remaining characters; returns self if an    y changes were made, nil otherwise:
+'
+
+puts s = 'hello World!' 
+puts s.capitalize!
+puts s 
+puts s.capitalize!
