1100cb639b9bc73c19ec11a7b37028879d51254b
Marcos Urbanski <marcos.urbanski@gmail.com>
2022-09-05 19:20:44 -0300

    .

--- Ruby/Curso-DIO-RUBY/strings.rb
+++ Ruby/Curso-DIO-RUBY/strings.rb
@@ -14,10 +14,13 @@ puts mensagem # impressão da variavel nome com aspas duplas.
 mensagem = 'Bem vindo #{ nome }'
 puts mensagem # impressão da variavel nome com apóstrofos, não irá sair o  valor da variavel. 
 
-mensagem = <<~ MENSAGEM 
+mensagem = <<~MENSAGEM 
     Olá #{ nome 
 
     Bem vindo(a) ao meu programa!
 
-    Obrigado.
+    Obrigado
+MENSAGEM
 }
+
+puts mensagem
