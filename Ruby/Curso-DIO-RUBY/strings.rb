5e0bfd5f21bb50b3f70ac4cf62f188cba6e57c5a
Marcos Urbanski <marcos.urbanski@gmail.com>
2022-09-05 19:31:25 -0300

    change strings.rb and close #112

--- Ruby/Curso-DIO-RUBY/strings.rb
+++ Ruby/Curso-DIO-RUBY/strings.rb
@@ -12,15 +12,31 @@ mensagem = "Bem vindo #{ nome }"
 puts mensagem # impressão da variavel nome com aspas duplas.
 
 mensagem = 'Bem vindo #{ nome }'
-puts mensagem # impressão da variavel nome com apóstrofos, não irá sair o  valor da variavel. 
+puts mensagem + " # impressão da variavel nome com apóstrofos, não irá sair o  valor da variavel. "
+puts "\n"
+
 
 mensagem = <<~MENSAGEM 
-    Olá #{ nome 
+    Olá #{ nome }
 
     Bem vindo(a) ao meu programa!
 
     Obrigado
 MENSAGEM
-}
 
+
+puts mensagem
+
+puts "\n"
+
+
+
+mensagem = %q(Bem vindo ao meu programa.)
+puts mensagem
+
+
+mensagem = %(Bem vindo ao meu programa #{nome} # %q não faz interpolação)
+puts mensagem
+
+mensagem = %Q(Bem vindo ao meu programa #{ nome } %Q imprime interporlação )
 puts mensagem
