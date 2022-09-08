a0b1b126bcd9dfd1288106e8bc5d6f0bbe83f759
Marcos Urbanski <marcos.urbanski@gmail.com>
2022-09-08 20:35:09 -0300

    add file issue37.rb and close #37

---
+++ Ruby/DOC/String/issue37.rb
@@ -0,0 +1,13 @@
+=begin
+
+
+chr → string
+Returns a string containing the first character of self:
+
+=end
+p "str.chr Returns a string containing the first character of self: "
+
+puts s = 'foo' # => "foo"
+puts s.chr     # => "f"
+
+
