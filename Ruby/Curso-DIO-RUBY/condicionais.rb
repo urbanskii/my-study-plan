93d9468a927f77c64dd2e2ffc1257aa6e2217e59
urbanskii <marcos.urbanski@gmail.com>
2022-09-09 15:37:41 -0300

    add file condicionais.rb

---
+++ Ruby/Curso-DIO-RUBY/condicionais.rb
@@ -0,0 +1,51 @@
+=begin
+Condicionais
+
+== # igualdade
+!= # diferente
+>  # maior que
+>= # maior ou igual
+<  # menor que
+<= menor ou igual
+
+!  # negação
+&& # and
+|| # or
+
+=end
+
+valor = 20
+if valor > 50
+  puts "Eu sou maior que 50"
+else
+  puts "Eu sou menor que 50"
+end
+
+puts 'teste'
+puts 'teste'
+puts 'teste'
+puts 'teste'
+
+
+
+
+puts 'teste'
+puts 'teste'
+puts 'teste'
+puts 'teste'
+
+puts 'teste'
+puts 'teste'
+puts 'teste'
+puts 'teste'
+
+puts 'teste'
+puts 'teste'
+puts 'teste'
+puts 'teste'
+
+puts 'teste'
+puts 'teste'
+puts 'teste'
+puts 'teste'
+
