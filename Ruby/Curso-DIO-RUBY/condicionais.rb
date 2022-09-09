6543900efaf5341e61ff7c3b73ef23947b62994b
urbanskii <marcos.urbanski@gmail.com>
2022-09-09 20:56:50 -0300

    s

--- Ruby/Curso-DIO-RUBY/condicionais.rb
+++ Ruby/Curso-DIO-RUBY/condicionais.rb
@@ -20,3 +20,21 @@ if valor > 50
 else
   puts "Eu sou menor que 50"
 end
+
+puts "Informe o valor: "
+valor = gets().chomp
+
+if valor != 10 || valor > 5 
+  p "este valor é diferente de 10 ou é maior que 5  #{valor}"
+else
+  p "o valor #{valor} não está dentro das condições //* valor != 10 || valor > 5 && (valor % 2) == 0 *//"
+end
+
+puts "Informe o valor: "
+valor = gets().chomp
+
+if (valor % 2) == 0
+  p "O numero informado é par: #{valor}"
+else
+  p "O numero informado é impar: #{valor}"
+end
