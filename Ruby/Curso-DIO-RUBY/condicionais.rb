9bb332685cf93ffccac3c5dab51a7e134632381a
urbanskii <marcos.urbanski@gmail.com>
2022-09-10 10:27:09 -0300

    change condicionais.rb

--- Ruby/Curso-DIO-RUBY/condicionais.rb
+++ Ruby/Curso-DIO-RUBY/condicionais.rb
@@ -27,14 +27,29 @@ valor = gets().chomp
 if valor != 10 || valor > 5 
   p "este valor é diferente de 10 ou é maior que 5  #{valor}"
 else
-  p "o valor #{valor} não está dentro das condições //* valor != 10 || valor > 5 && (valor % 2) == 0 *//"
+  p "o valor #{valor} não está dentro das condições //* valor != 10 || valor > 5 *//"
 end
 
-puts "Informe o valor: "
-valor = gets().chomp
+valor = valor.to_i
 
-if (valor % 2) == 0
-  p "O numero informado é par: #{valor}"
+p valor.class
+
+if valor > 50
+  puts "Eu sou maior que 50"
+elsif valor == 50
+  puts "Eu sou igual a 50"
 else
-  p "O numero informado é impar: #{valor}"
+  puts "Eu sou menor que 50"
 end
+
+valor = 30
+puts "Eu sou maior que 50" if valor > 50
+
+unless 2 != 2
+  puts "utilizar unless quando a condição não for verdadeira"
+end
+
+
+
+valor = gets.chomp.to_i
+
