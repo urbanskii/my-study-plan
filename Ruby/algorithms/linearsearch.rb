0a41dc74a4bcc13b89ee8a06ace405c8ba1acc6e
urbanskii <marcos.urbanski@gmail.com>
2022-09-13 06:19:25 -0300

    change file linearsearch.rb close #136

--- Ruby/algorithms/linearsearch.rb
+++ Ruby/algorithms/linearsearch.rb
@@ -12,18 +12,24 @@ def search(arr, x)
   n = arr.length 
   i = 0
   a = -1
+  
   arr.each do i      
     if arr[i] == x        
-        a= 1
-        p "valor: #{arr[i]}"
-        break
+      a= 1        
+      break
     end    
     i += 1   
   end
-  a   
+  a 
 end
 
 
 arr = [2,3,4,10,40]
 x = 40
-p "valor do search: #O valor mencionado está no index: #{}{search(arr, x)}"
+retorno = [search(arr, x)]
+
+if retorno[0] == 1
+  p "Valor: #{x} foi encontrado."
+else 
+  p "Valor não foi encontrado na pesquisa!"
+end
