736a1dee73628a7c967badf5428146ccb2fd11fc
urbanskii <marcos.urbanski@gmail.com>
2022-09-13 07:17:47 -0300

    add linearsearch.rb

---
+++ Ruby/algorithms/binarysearch.rb
@@ -0,0 +1,21 @@
+=begin
+
+
+Algoritmo para busca linear em um array.
+
+
+=end
+
+def search(arr, x)
+  low = 0
+  high
+    
+
+
+end
+
+
+arr = [2,3,4,10,40].sort
+x = 10
+
+p "search: #{search(arr, x)}"
