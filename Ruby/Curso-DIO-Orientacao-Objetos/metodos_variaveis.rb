deddbeb4f3662a34d1c080465d2359408fc29923
urbanskii <marcos.urbanski@gmail.com>
2022-09-14 20:33:51 -0300

    add file metodos_variaveis.rb

---
+++ Ruby/Curso-DIO-Orientacao-Objetos/metodos_variaveis.rb
@@ -0,0 +1,14 @@
+=begin
+
+Colocar o @ antes do nome da variavel, torna a mesma disponivel dentro do ciclo de vida do objeto, tornando ela utilizavel em todos os metodos da classe.
+
+metodo initialize vai
+
+
+class NomeDaClasse
+  def imprimir_ola(nome)
+
+
+
+
+=end
