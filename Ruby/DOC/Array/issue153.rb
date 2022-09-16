df9613a6a023e0287d17563709eef574a7413f5f
urbanskii <marcos.urbanski@gmail.com>
2022-09-16 20:08:52 -0300

    change file issue153

--- Ruby/DOC/Array/issue153.rb
+++ Ruby/DOC/Array/issue153.rb
@@ -20,3 +20,38 @@ a # => [:foo, "bar", 2]
 p "Foi criado um novo array a = Array.new([:foo, 'bar', 2])"
 p "variavel a representa o array criado: #{a.class}"
 p "Os dados criados no array: #{a}"
+
+p "**" * 75
+
+p "With no block and a single Integer argument size, returns a new Array of the given size whose elements are all nil:"
+
+
+a = Array.new(3)
+a # => [nil, nil, nil]
+
+
+p "Array criado e o seu tamanho: #{a.length}  -  #{a.class} porém com elementos vazios nil"
+
+p "**" * 75
+
+p "With no block and arguments size and default_value, returns an Array of the given size; each element is that same default_value:"
+
+a = Array.new(3, 'x')
+a # => ['x', 'x', 'x']
+
+p "Array criado: #{a} "
+
+p "**" * 75
+
+p "With a block and argument size, returns an Array of the given size; the block is called with each successive integer index; the element for that index is the return value from the block:"
+
+a = Array.new(3) {|index| "Element #{index}" }
+a # => ["Element 0", "Element 1", "Element 2"]
+
+p "#{a}"
+
+p "**" * 75
+
+
+
+p "**" * 75
