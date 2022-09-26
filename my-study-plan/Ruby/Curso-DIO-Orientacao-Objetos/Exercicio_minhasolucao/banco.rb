59c3643ffcc14455e25e5452ed3f14e2a24325c1
urbanskii <marcos.urbanski@gmail.com>
2022-09-26 07:51:14 -0300

    metodo transfere não funcionou.

--- my-study-plan/Ruby/Curso-DIO-Orientacao-Objetos/Exercicio_minhasolucao/banco.rb
+++ my-study-plan/Ruby/Curso-DIO-Orientacao-Objetos/Exercicio_minhasolucao/banco.rb
@@ -7,18 +7,17 @@ conta2 = Conta.new
 p conta1
 p conta2
 
-conta1.conta("marcos", 100)
+conta1.conta("marcos", 200)
 #conta1.consulta
 
-conta2.conta("joao", 200)
+conta2.conta("joao", 100)
 #conta2.consulta
 
-p conta1.cliente
-p conta2.cliente
+valor = 0
 
 
 
-#conta2.executa_transfere(50, conta1.executa_deposita(50))
+conta1.transfere(conta2, valor)
 
 p "Conta 1 saldo: #{conta1.consulta_saldo}"
 p "Conta 2 saldo: #{conta2.consulta_saldo}"
