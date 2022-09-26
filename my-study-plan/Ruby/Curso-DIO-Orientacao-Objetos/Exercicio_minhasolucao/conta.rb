95df82d3f5d06a88918dc5c54b781732cf9d9e63
urbanskii <marcos.urbanski@gmail.com>
2022-09-26 07:51:17 -0300

    metodo transfere nao funcionou close #147

--- my-study-plan/Ruby/Curso-DIO-Orientacao-Objetos/Exercicio_minhasolucao/conta.rb
+++ my-study-plan/Ruby/Curso-DIO-Orientacao-Objetos/Exercicio_minhasolucao/conta.rb
@@ -1,68 +1,38 @@
 require_relative "./pessoa"
 
  class Conta
-    @id = 0
-    @saldo = 0
-    @nome = nil
-
     def conta(nome, valor) #metodo publico
-        puts "Método principal"
-        id(nome, valor) #encapsulado dentro do metodo publico para ser chamado pois é private
-        #transfere #encapsulado dentro do metodo publico para ser chamado pois é private   
-        
-    end
-
-    def executa_transfere(valor, conta)
-        transfere(valor)
-
-    end
-
-    def executa_deposita(valor)
-        deposita(valor)
-    end
-
-    def consulta_saldo
-       @saldo
-    end
-    def cliente
-        p @id
-        p @saldo
-        p @nome
-        
-    end
-
-    def id(nome, valor)
-        puts "cria nova conta"
-        @id += 1
+        puts "Método principal"     
+        #transfere #encapsulado dentro do metodo publico para ser chamado pois é private  
+        @saldo = 0
         pessoa = Pessoa.new(nome)
         @nome = pessoa.nome
-        deposita(valor)
+        @saldo += valor
         
     end
+  
+    def consulta_saldo
+       p @saldo
+    end
 
- 
-
-    def transfere(valor)
+    def transfere(conta, valor)
         puts "transfere"
         @saldo -= valor
     end
-
-    def deposita(valor)
-        @saldo += valor
-    end
-
+   
     def saca(valor)
         consulta_saldo(valor)
     end
 
-    def deposita(valor)
+    def deposita(conta, valor)
         puts "deposita"
-        @saldo += valor
+        p valor
+        @saldo += valor        
     end
 
     protected # tudo que é criado abaixo é protected
     def consulta(valor)
-        puts "Saldo"
+        puts "@saldo"
         @saldo = valor
     end
 end
