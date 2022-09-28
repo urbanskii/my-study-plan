e4c7bf069bb786c5fc024dac1158ffd9ad9c3e52
urbanskii <marcos.urbanski@gmail.com>
2022-09-28 15:43:57 -0300

    change file bubblesortsearch_my.rb

--- my-study-plan/algorithms/bubblesortsearch_my.rb
+++ my-study-plan/algorithms/bubblesortsearch_my.rb
@@ -1,40 +1,32 @@
 
-
-arr = [5,1,4,2,8]
+arr = [5,4,2,8]
+arr1 = [5,4,2,8]
 valor = 0 
 condition = false
 j = 0
 i = 0
 
 for j in arr
-    p "valor de j: #{j}"
-    p "valor de arr,valor: #{arr[valor]}"
-
-    p "valor de arr,valor+1: #{arr[valor+1]}"
-    p "Array: #{arr}"
-
-    if arr[valor] > arr[valor+1]          
-            arr[valor], arr[valor+1] = arr[valor+1], arr[valor] 
-            valor += 1  
-            p arr[valor+1] 
-    end
-    
-    
-              #if arr[valor] == nil
-         #   break
-        #end
-end
-valor = 0
-
-p arr
-
-for i in arr
     if arr[valor] > arr[valor+1]          
         arr[valor], arr[valor+1] = arr[valor+1], arr[valor] 
-        valor += 1  
-        p arr[valor+1] 
+        valor += 1             
     end
 end
 
+valor = 0
 
+for i in arr   
+    if arr[valor+1] == nil
+        break
+    end
+    if arr[valor] > arr[valor+1]          
+        arr[valor], arr[valor+1] = arr[valor+1], arr[valor] 
+        valor += 1         
+    else
+        valor += 1
+    end   
+end
+
+p "Array antes: #{arr1}"
+p "Final do array: #{arr}"
         
