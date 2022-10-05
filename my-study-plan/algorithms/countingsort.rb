5f4664a6305046bf2b86242699d6052791940df6
urbanskii <marcos.urbanski@gmail.com>
2022-10-05 16:01:49 -0300

    feat: edit file countingsort.rb #139

---
+++ my-study-plan/algorithms/countingsort.rb
@@ -0,0 +1,28 @@
+
+
+
+
+def counting_sort(arr)
+    i = 0
+    arr_counting = 0
+
+    while counting = false
+        arr.each do i
+        p i         
+        if arr[i] > arr[i+1]
+            arr_counting >= arr[i]
+                arr_counting = arr[i]
+        end
+        i += 1
+        if arr[i+1] == nil
+            counting = true
+        end
+    end
+
+end
+
+
+arr= [1,5,1,7,4,8,6]
+
+
+p counting_sort(arr)
