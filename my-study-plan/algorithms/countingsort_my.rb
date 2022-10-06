f367538d202d7fbdd68c605a2fb7b6513327f7a5
urbanskii <marcos.urbanski@gmail.com>
2022-10-06 12:14:30 -0300

    feat: change file countingsort.rb #139

---
+++ my-study-plan/algorithms/countingsort_my.rb
@@ -0,0 +1,54 @@
+
+
+
+
+def counting_sort(arr)
+    i = 0
+    arr_counting = 0
+    counting = false
+
+
+    while counting == false          
+        if arr[i] < arr[i+1]
+            arr_counting = arr[i+1]        
+        end        
+        i += 1
+        if arr[i+1] == nil
+            counting = true
+        end        
+    end  
+ 
+
+    arr_counting_size = Array.new(arr_counting+1, 0)
+    i =0
+
+    arr_counting_size.each do |chave|  
+        arr.each do |chave1, valor|
+            if chave1 == i
+                arr_counting_size[i] += 1
+            end
+        end
+        i += 1        
+    end
+
+    i=0
+    p arr_counting_size
+   
+    arr_counting_size.size.times do |number|
+        arr_counting_size[number].times do
+            p i
+            i =+ 1
+    end
+    p arr_counting_size
+
+    
+
+
+
+end
+
+
+arr= [1,3]
+
+
+p counting_sort(arr)
