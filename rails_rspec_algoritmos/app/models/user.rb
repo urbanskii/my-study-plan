9d75b87f695a7f09bf8e13370365dd3f9865153b
urbanskii <marcos.urbanski@gmail.com>
2022-10-08 18:12:27 -0300

    feat: creat user_spec tests.

--- rails_rspec_algoritmos/app/models/user.rb
+++ rails_rspec_algoritmos/app/models/user.rb
@@ -1,2 +1,5 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base 
+    validates :firstname, presence: true 
+    validates :lastname, presence: true 
+    validates :email, presence: true, uniqueness: true 
 end
--- rails_rspec_algoritmos/spec/models/user_spec.rb
+++ rails_rspec_algoritmos/spec/models/user_spec.rb
@@ -1,5 +1,30 @@
-require 'rails_helper'
+require 'rails_helper' 
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do 
+ it "It's valid when contains the first name, last name and e-mail" do
+  user = User.new(firstname: 'Bruce',
+   lastname: 'Dickinson', 
+   email:'bruce@ironmaiden.com')
+   
+   expect(user).to be_valid
+  end
 end
+
+describe User do
+ it "It's not valid when the first name is nil " do
+  user = User.new(firstname: nil)
+  user.valid?
+  expect(user.errors[:firstname].to include("can't be blank"))
+ end
+end
+
+describe User do
+  it "It's invalid it it already exists an equal e-mail" do 
+    user = User.create( firstname: 'Steve',
+     lastname: 'Harris', email: 'contato@ironmaiden.com' )
+    user = User.new( firstname: 'Bruce',
+     lastname: 'Dickinson', email: 'contato@ironmaiden.com' )
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
+    end
+  end
