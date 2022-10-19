a5326fb70ff07c23878ba65a8eba7b8d44fd106b
urbanskii <marcos.urbanski@gmail.com>
2022-10-19 18:17:09 -0300

    Minimum Sum, básico  #215

--- my-study-plan/algorithms/minimum_spanning.rb
+++ my-study-plan/algorithms/minimum_spanning.rb
@@ -7,15 +7,19 @@ Minimum sum by choosing minimum of pairs from array
     
 =end
 
-A = [2,4,1,3]
+A = [2,4,1,3,6,8,1]
 B = []
 i = 0
 
+array_size = A.size
+
+p array_size
+p A.each
+
 A.each do i
-    B[i] = A.min
-    A.pop(B[i])
-    i += 1
+    min_value = A.min
+    A.pop(min_value)
+    p A  
+   
 end
 
-p A
-p B
