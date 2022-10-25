06cefc40dd0ecda53e6f4c35ba76a0567bf7c4f6
urbanskii <marcos.urbanski@gmail.com>
2022-10-25 20:06:21 -0300

    feat: add findThree.rb #223

---
+++ my-study-plan/algorithms/findThree.rb
@@ -0,0 +1,12 @@
+=begin
+    
+Find the largest three distinct elements in an array
+
+Given an array with all distinct elements, find the largest three elements. Expected time complexity is O(n) and extra space is O(1). 
+
+Examples :
+
+Input: arr[] = {10, 4, 3, 50, 23, 90}
+Output: 90, 50, 23
+    
+=end
