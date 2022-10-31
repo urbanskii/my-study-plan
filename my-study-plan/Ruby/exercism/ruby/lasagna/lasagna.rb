d16b7e0f07ad46017b670b19b7b7f8c2cc689993
urbanskii <marcos.urbanski@gmail.com>
2022-10-31 19:11:51 -0300

    feat: featureissue268-lasagna-04 close #268

--- my-study-plan/Ruby/exercism/ruby/lasagna/lasagna.rb
+++ my-study-plan/Ruby/exercism/ruby/lasagna/lasagna.rb
@@ -1,17 +1,14 @@
 class Lasagna
   EXPECTED_MINUTES_IN_OVEN = 40
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-    return EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
-    raise 'Please implement the Lasagna#remaining_minutes_in_oven method'
+    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-    layers = layers*layers
-    return layers
-    raise 'Please implement the Lasagna#preparation_time_in_minutes method'
+    2*layers
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
-    raise 'Please implement the Lasagna#total_time_in_minutes method'
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
   end
 end
