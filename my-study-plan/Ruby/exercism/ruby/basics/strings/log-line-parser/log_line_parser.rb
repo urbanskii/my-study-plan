1db1686881bc13d63cbf7ae9c1bfa42dd62691e3
urbanskii <marcos.urbanski@gmail.com>
2022-11-03 18:43:28 -0300

    feat: fix issue #280

--- my-study-plan/Ruby/exercism/ruby/basics/strings/log-line-parser/log_line_parser.rb
+++ my-study-plan/Ruby/exercism/ruby/basics/strings/log-line-parser/log_line_parser.rb
@@ -7,29 +7,23 @@ class LogLineParser
     @line = @line.lstrip
     first_index = @line.index(':')
     second_index = @line.size
-    @line = @line.slice(first_index+2, second_index)
+    @line = @line.slice(first_index + 2, second_index)
     @line.strip
   end
 
   def log_level
-    log= ''
+    return unless @line.downcase.match?('error') || @line.downcase.match?('info') || @line.downcase.match?('warning')
 
-    if @line.downcase.match?('error') == true or @line.downcase.match?('info') == true or @line.downcase.match?('warning') == true
-      first_index = @line.index(']')
-      @line = @line.downcase.slice(1, first_index)
-      @line = @line.chomp(']')
-      @line.strip
-    end
+    first_index = @line.index(']')
+    @line = @line.downcase.slice(1, first_index)
+    @line = @line.chomp(']')
+    @line.strip
   end
 
   def reformat
     messages = @line
-    log= log_level
+    log = log_level
     @line = messages
     @line = "#{message} (#{log})"
   end
 end
-
-
-p LogLineParser.new("[WARNING]:   \tTimezone not set  \r\n").message
-p LogLineParser.new("[ERROR]: \t Corrupt disk\t \t \r\n").reformat
