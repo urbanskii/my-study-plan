e327c5cb897dc281ad65cd3752be272d4f8ebc44
urbanskii <marcos.urbanski@gmail.com>
2022-11-03 18:15:50 -0300

    feat: fix issue #282

--- my-study-plan/Ruby/exercism/ruby/basics/strings/log-line-parser/log_line_parser.rb
+++ my-study-plan/Ruby/exercism/ruby/basics/strings/log-line-parser/log_line_parser.rb
@@ -1,17 +1,34 @@
 class LogLineParser
   def initialize(line)
-    @line = line.slice(10, 23)
+    @line = line
   end
 
   def message
-    @line
+    @line = @line.lstrip
+    first_index = @line.index(':')
+    second_index = @line.size
+    @line = @line.slice(first_index+2, second_index)
+    @line = @line.chomp('\t')
   end
 
   def log_level
-    raise 'Please implement the LogLineParser#log_level method'
+    if @line.downcase.match?('error') == true or @line.downcase.match?('info') == true or @line.downcase.match?('warning') == true
+      first_index = @line.index(']')
+      @line = @line.downcase.slice(1, first_index)
+      @line = @line.chomp(']')
+    end
   end
 
   def reformat
     raise 'Please implement the LogLineParser#reformat method'
   end
 end
+
+
+p LogLineParser.new('[ERROR]: Stack overflow').message
+p LogLineParser.new('[WARNING]: Disk almost full').message
+p LogLineParser.new('[INFO]: File moved').message
+p LogLineParser.new("[WARNING]:   \tTimezone not set  \r\n").message
+
+p LogLineParser.new('[ERROR]: Disk full').log_level
+p LogLineParser.new('[WARNING]: Unsafe password').log_level
