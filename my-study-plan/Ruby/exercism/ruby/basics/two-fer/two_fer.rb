ec9cba6de94ecb1061265faa97b0407c8540cd57
urbanskii <marcos.urbanski@gmail.com>
2022-11-04 19:21:19 -0300

    feat: add exercise two-fer #288

---
+++ my-study-plan/Ruby/exercism/ruby/basics/two-fer/two_fer.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Two Fer' exercise in this file. Make the tests in
+`two_fer_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/two-fer` directory.
+=end
