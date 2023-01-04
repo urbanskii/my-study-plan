4452a60fb4c904749feaba436435d3b64c181bbe
urbanskii <marcos.urbanski@gmail.com>
2023-01-04 18:58:21 -0300

    feat: add branch 309-isogram, close #309

--- my-study-plan/Ruby/exercism/ruby/basics/strings/isogram/isogram.rb
+++ my-study-plan/Ruby/exercism/ruby/basics/strings/isogram/isogram.rb
@@ -4,4 +4,27 @@ Write your code for the 'Isogram' exercise in this file. Make the tests in
 
 To get started with TDD, see the `README.md` file in your
 `ruby/isogram` directory.
+
+this code is an implementation from chatGPT
 =end
+
+class Isogram
+  def self.isogram?(input)
+    @input = input
+
+    #gsub to remove hyphens and spaces
+    clean_word = input.gsub(/[-\s]/, '')
+
+    #convert clean_word to lowercase, create an chars array and remove any duplicate letter with uniq.
+    # check with length if the result is the same after the methods.
+    clean_word.downcase.chars.uniq.length == clean_word.length
+
+  end
+end
+
+input = "Emily Jung Schwartzkopf"
+testiso = Isogram.isogram?(input)
+
+puts testiso
+
+
